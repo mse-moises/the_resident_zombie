ba15024ce3fdb8526fe3034faeeab356381409f2
Moises <moisesalonso01@gmail.com>
2021-11-25 01:15:37 -0300

    test: test for user model 'from json' factory

--- test/features/user/data/models/use_model_test.dart
+++ test/features/user/data/models/use_model_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -5,11 +7,27 @@ import 'package:mockito/mockito.dart';
 import 'package:the_resident_zombie/features/user/data/models/user_model.dart';
 import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 
+import '../../../../fixture/fixture_reader.dart';
+
 void main() {
-  final tUserModel = UserModel(name: 'test', age: 1, gender: 'test');
+  final tUserModel = UserModel(name: 'Fabio Akita', age: 30, gender: 'M');
 
   test('userModel should be a subclass of UserEntity', () async {
     // assert
     expect(tUserModel, isA<UserEntity>());
   });
+
+  group('fromJson', () {
+    test('return a valid model from JSON',
+      () async {
+        // arrange
+        final Map<String,dynamic>  jsonMap = json.decode(fixture('user.json'));
+        // act
+        final result = UserModel.fromJson(jsonMap);
+        // assert
+        expect(result, tUserModel);
+      }
+    );
+  }
+  );
 }
