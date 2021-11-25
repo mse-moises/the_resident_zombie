291c9a7c73ac898b76ddaa2fb33d2c1b18606644
Moises <moisesalonso01@gmail.com>
2021-11-25 00:47:16 -0300

    refactor: add params to the usecase

--- lib/features/user/domain/usecases/create_user_usecase.dart
+++ lib/features/user/domain/usecases/create_user_usecase.dart
@@ -1,14 +1,28 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:the_resident_zombie/core/error/failures.dart';
+import 'package:the_resident_zombie/core/usecases/usecase.dart';
 import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 import 'package:the_resident_zombie/features/user/domain/repositories/user_repository.dart';
 
-class CreateUserUsecase{
+class CreateUserUsecase implements UseCase<UserEntity, Params> {
   final UserRepository repository;
 
   CreateUserUsecase(this.repository);
 
-  Future<Either<Failure,UserEntity>> call({required String name, required int age, required String gender}) async {
-    return await repository.createUser(name, age, gender);
+  Future<Either<Failure, UserEntity>> call(
+      Params params) async {
+    return await repository.createUser(params.name, params.age, params.gender);
   }
+}
+
+class Params extends Equatable {
+  final String name;
+  final int age;
+  final String gender;
+
+  Params({required this.name, required this.age, required this.gender});
+
+  @override
+  List<Object> get props => [name, age, gender];
 }
