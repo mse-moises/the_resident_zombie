110bd2aad8d378763e41d6c50667bfcd2cb9a843
Moises <moisesalonso01@gmail.com>
2021-11-25 19:18:00 -0300

    test: add user model test

--- test/features/user/data/models/use_model_test.dart
+++ test/features/user/data/models/use_model_test.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 
-import 'package:dartz/dartz.dart';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+
 import 'package:the_resident_zombie/features/user/data/models/user_model.dart';
 import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 
