ed62d374eba709aa6e5dcff871e7908f6c92d6b0
Moises <moisesalonso01@gmail.com>
2021-11-25 01:32:25 -0300

    feat: toJson method add to usermodel

--- lib/features/user/data/models/user_model.dart
+++ lib/features/user/data/models/user_model.dart
@@ -3,4 +3,22 @@ import 'package:the_resident_zombie/features/user/domain/entities/user_entity.da
 class UserModel extends UserEntity {
   UserModel({required String name, required int age, required String gender})
       : super(name:name, age:age, gender:gender);
+
+  factory UserModel.fromJson(Map<String,dynamic> json){
+    return UserModel(
+      name:json['name'],
+      age:json['age'],
+      gender:json['gender'],
+    );
+  }
+
+  Map<String,dynamic> toJson(){
+    return{
+      'name':name,
+      'age':age,
+      'gender':gender
+    };
+  }
+
+
 }
