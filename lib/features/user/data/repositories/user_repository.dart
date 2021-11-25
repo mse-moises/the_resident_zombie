6314141a052f8dac509404d84471c8b1d63278a8
Moises <moisesalonso01@gmail.com>
2021-11-25 19:19:12 -0300

    feat: add user repositories implementation

--- lib/features/user/data/repositories/user_repository.dart
+++ lib/features/user/data/repositories/user_repository.dart
@@ -1,13 +1,39 @@
+import 'package:the_resident_zombie/core/error/exceptions.dart';
+import 'package:the_resident_zombie/core/platform/network_info.dart';
+import 'package:the_resident_zombie/features/user/data/datasources/user_cache_data_source.dart';
+import 'package:the_resident_zombie/features/user/data/datasources/user_remote_data_source.dart';
 import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 import 'package:the_resident_zombie/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:the_resident_zombie/features/user/domain/repositories/user_repository.dart';
 
-class UserRepositoryImpl implements UserRepository{
-  @override
-  Future<Either<Failure, UserEntity>> createUser(String name, int age, String gender) {
-    // TODO: implement createUser
-    throw UnimplementedError();
-  }
+class UserRepositoryImpl implements UserRepository {
+  final UserRemoteDataSource remoteDataSource;
+  final UserCacheDataSource cacheDataSource;
+  final NetworkInfo networkInfo;
 
+  UserRepositoryImpl({
+    required this.remoteDataSource,
+    required this.cacheDataSource,
+    required this.networkInfo,
+  });
+
+  @override
+  Future<Either<Failure, UserEntity>> createUser(
+    String name,
+    int age,
+    String gender,
+  ) async {
+    if (!await networkInfo.isConnected) return Left(ServerFailure());
+
+    try {
+      final remoteAddress =
+          await remoteDataSource.createUser(name, age, gender);
+
+      cacheDataSource.cacheUser(remoteAddress);
+      return Right(remoteAddress);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
 }
