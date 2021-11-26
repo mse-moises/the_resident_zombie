be75379d6f06874cd86b95cd1cbe8057b47aab9a
Moises <moisesalonso01@gmail.com>
2021-11-26 01:09:12 -0300

    feat: implementation of user remote data source

--- lib/features/user/data/datasources/user_cache_data_source.dart
+++ lib/features/user/data/datasources/user_cache_data_source.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:the_resident_zombie/core/error/exceptions.dart';
 import 'package:the_resident_zombie/features/user/data/models/user_model.dart';
 
 abstract class UserCacheDataSource{
@@ -7,4 +11,30 @@ abstract class UserCacheDataSource{
   Future<UserModel> getUser();
 
   Future<void> cacheUser(UserModel userToCache);
+}
+
+const String CACHED_USER = 'CACHED_USER';
+
+
+class UserCacheDataSourceImpl implements UserCacheDataSource{
+  final SharedPreferences sharedPreferences;
+
+  UserCacheDataSourceImpl({required this.sharedPreferences});
+  @override
+  Future<void> cacheUser(UserModel userToCache) async {
+    sharedPreferences.setString(CACHED_USER, userToCache.toString());
+  }
+
+  @override
+  Future<UserModel> getUser() async {
+     final jsonString = sharedPreferences.getString(CACHED_USER);
+
+    if(jsonString!=null){
+      return await Future.value(UserModel.fromJson(json.decode(jsonString)));
+    }else{
+      throw CacheException();
+    }
+
+  }
+
 }
--- lib/features/user/data/datasources/user_remote_data_source.dart
+++ lib/features/user/data/datasources/user_remote_data_source.dart
@@ -1,6 +1,10 @@
-import 'package:the_resident_zombie/features/user/data/models/user_model.dart';
+import 'dart:convert';
 
-abstract class UserRemoteDataSource{
+import 'package:the_resident_zombie/core/error/exceptions.dart';
+import 'package:the_resident_zombie/features/user/data/models/user_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class UserRemoteDataSource {
   ///Send a http post request for the http://zssn-backend-example.herokuapp.com/api/people.json endpoint.
   ///
   ///
@@ -8,4 +12,32 @@ abstract class UserRemoteDataSource{
 
   @override
   Future<UserModel> createUser(String name, int age, String gender);
+}
+
+class UserRemoteDataSourceImpl implements UserRemoteDataSource {
+  final http.Client client;
+
+  UserRemoteDataSourceImpl({required this.client});
+  @override
+  Future<UserModel> createUser(String name, int age, String gender) async {
+    final body = {
+      "person": {
+        "name": "$name",
+        "age": age.toString(),
+        "gender": "$gender"
+      }
+    };
+
+    final response = await client.post(
+      Uri.parse('http://zssn-backend-example.herokuapp.com/api/people.json'),
+      headers: {'Content-Type': 'application/json'},
+      body:body,
+    );
+
+    if (response.statusCode == 200) {
+      return UserModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
