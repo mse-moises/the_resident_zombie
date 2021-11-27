d927ee408c0059161e6b5b53caedadeb9fb9315a
Moises <moisesalonso01@gmail.com>
2021-11-27 16:20:27 -0300

    feat; add item entity and backpack entity

---
+++ lib/features/items/domain/entities/back_pack_entity.dart
@@ -0,0 +1,65 @@
+import 'package:equatable/equatable.dart';
+import 'package:the_resident_zombie/features/items/domain/entities/item_entity.dart';
+
+class BackPackEntity extends Equatable {
+  List<ItemEntity> items = [];
+
+  void add(ItemEntity itemEntity) {
+    items.add(itemEntity);
+  }
+
+  @override
+  String toString() {
+    List<ItemEntity> allEntities = []..addAll(items);
+
+    List<List<ItemEntity>> allSameEntities = [];
+
+    for (int i = allEntities.length - 1;
+        i >= 0 && allEntities.isNotEmpty;
+        i--) {
+      final itemEntity = allEntities[i];
+      // get the name of the item
+      String firstItemName = itemEntity.name;
+      // array that gonna contain all the item with the same name
+      final sameItems = <ItemEntity>[];
+
+      // travel throught the list searching for items with the same name
+      for (int i = allEntities.length - 1;
+          i >= 0 && allEntities.isNotEmpty;
+          i--) {
+        final itemEntityAux = allEntities[i];
+
+        //test if is the same name
+        if (itemEntityAux.name == firstItemName) {
+          sameItems.add(itemEntityAux);
+          //remove from the allEntities list so it cannot be counted again
+          allEntities.remove(itemEntityAux);
+        }
+      }
+
+      allSameEntities.add(sameItems);
+    }
+    // should have a array of subarrays with the same itens
+
+    // sort list  by list with more items to list with less items
+    allSameEntities.sort((a, b) => b.length.compareTo(a.length));
+
+    String stringToReturn = "";
+
+    for (int i = 0; i < allSameEntities.length; i++) {
+      List<ItemEntity> list = allSameEntities[i];
+      final string = "${list[0].name}:${list.length}";
+
+      if (i != 0) {
+        stringToReturn += ";$string";
+      } else {
+        stringToReturn += string;
+      }
+    }
+
+    return stringToReturn;
+  }
+
+  @override
+  List<Object> get props => [items];
+}
---
+++ lib/features/items/domain/entities/item_entity.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class ItemEntity extends Equatable{
+  final String name;
+  final int value;
+
+  ItemEntity({required this.name, required this.value});
+
+  @override
+  List<Object> get props => [name,value];
+}
