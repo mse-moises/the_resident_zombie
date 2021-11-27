f06f77758ddb2e76af6b5526ff714f6d92d12b7a
Moises <moisesalonso01@gmail.com>
2021-11-27 17:33:08 -0300

    feat: add get items type usecase

---
+++ lib/features/items/domain/usecase/get_items_type_usecase.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:the_resident_zombie/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:the_resident_zombie/core/usecases/usecase.dart';
+import 'package:the_resident_zombie/features/items/domain/entities/back_pack_entity.dart';
+import 'package:the_resident_zombie/features/items/domain/entities/item_entity.dart';
+import 'package:the_resident_zombie/features/items/domain/repository/items_repository.dart';
+
+class GetItemsTypeUseCase implements UseCase<List<ItemEntity>, NoParams>{
+  final ItemsRepository repository;
+
+  GetItemsTypeUseCase(this.repository);
+  @override
+  Future<Either<Failure, List<ItemEntity>>> call(params) async {
+    return await repository.getItemsType();
+  }
+
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object> get props => [];
+}
