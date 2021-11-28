e1a12e0b101118697aa2b5e7f22863b36e406f8f
Moises <moisesalonso01@gmail.com>
2021-11-28 17:11:46 -0300

    test: add test for the implementation of saveContact method on local data source

--- test/features/user/data/datasources/user_local_data_source_test.dart
+++ test/features/user/data/datasources/user_local_data_source_test.dart
@@ -27,15 +27,13 @@ void main() {
     'User local datasource:',
     () {
       final userJson = fixture('user_cached.json');
-      final tUserModel =
-          UserModel.fromJson(json.decode(userJson));
+      final tUserModel = UserModel.fromJson(json.decode(userJson));
 
       test(
         'return User data from shared preferences when there is one cached',
         () async {
           // arrange
-          when(mockSharedPreferences.getString(any))
-              .thenReturn(userJson);
+          when(mockSharedPreferences.getString(any)).thenReturn(userJson);
           // act
           final result = await datasource.getUser();
           // assert
@@ -53,6 +51,77 @@ void main() {
           final call = datasource.getUser;
           // assert
           expect(() => call(), throwsA(TypeMatcher<CacheException>()));
+        },
+      );
+
+      group(
+        'Save connection -',
+        () {
+          final tIdentifier = 'test';
+
+          test(
+            'return the tIdentifier if shared preferences return null',
+            () async {
+              // arrange
+              when(mockSharedPreferences.getString(any)).thenReturn(null);
+              when(mockSharedPreferences.setString(any, any))
+                  .thenAnswer((_) async => true);
+              // act
+              final result = await datasource.saveContact(tIdentifier);
+              // assert
+              verify(mockSharedPreferences.getString(CACHED_CONTACT));
+              verify(
+                  mockSharedPreferences.setString(CACHED_CONTACT, tIdentifier));
+              expect(result, equals(tIdentifier));
+            },
+          );
+
+          test(
+            'return the tIdentifier if shared preferences return a string',
+            () async {
+              // arrange
+              when(mockSharedPreferences.getString(any)).thenReturn('test');
+              when(mockSharedPreferences.setString(any, any))
+                  .thenAnswer((_) async => true);
+              // act
+              final result = await datasource.saveContact(tIdentifier);
+              // assert
+              verify(mockSharedPreferences.getString(CACHED_CONTACT));
+              verify(
+                  mockSharedPreferences.setString(CACHED_CONTACT, "$tIdentifier;$tIdentifier"));
+              expect(result, equals(tIdentifier));
+            },
+          );
+
+          test(
+            'throw a [CacheException] if the sharedPreferences return false when its set',
+            () async {
+              // arrange
+              when(mockSharedPreferences.getString(any)).thenReturn(null);
+              when(mockSharedPreferences.setString(any, any))
+                  .thenThrow(CacheException());
+              // act
+              final call = () => datasource.saveContact(tIdentifier);
+              // assert
+
+              expect(() => call(), throwsA(TypeMatcher<CacheException>()));
+            },
+          );
+
+          test(
+            'throw a [CacheException] if the sharedPreferences thows an error on get',
+            () async {
+              // arrange
+              when(mockSharedPreferences.getString(any)).thenThrow(CacheException());
+              when(mockSharedPreferences.setString(any, any))
+                  .thenThrow(CacheException());
+              // act
+              final call = () => datasource.saveContact(tIdentifier);
+              // assert
+
+              expect(() => call(), throwsA(TypeMatcher<CacheException>()));
+            },
+          );
         },
       );
     },
