00f325496f74a77f885e81a1470dd985c5c2a9ba
Moises <moisesalonso01@gmail.com>
2021-11-27 21:02:37 -0300

    refact: transform file getter in a class

--- lib/core/platform/file_getter.dart
+++ lib/core/platform/file_getter.dart
@@ -1,3 +1,13 @@
 import 'dart:io';
 
-String getFile(String name) => File('assets/json/$name').readAsStringSync();
+abstract class FileGetter {
+  Future<String> getFile(String name);
+}
+
+class FileGetterImpl implements FileGetter {
+  @override
+  Future<String> getFile(String name) {
+    return Future.value(File('assets/json/$name').readAsStringSync());
+
+  }
+}
