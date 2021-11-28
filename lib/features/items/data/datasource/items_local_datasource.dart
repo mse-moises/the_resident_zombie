d9e676a1632e6e44431ce103cf5f54c14606f90e
Moises <moisesalonso01@gmail.com>
2021-11-28 05:06:14 -0300

    refactor: move the item type file reference to a constant

--- lib/features/items/data/datasource/items_local_datasource.dart
+++ lib/features/items/data/datasource/items_local_datasource.dart
@@ -10,13 +10,14 @@ abstract class ItemsLocalDataSource {
   Future<List<ItemEntity>> getItemsType();
 }
 
+const String itemsFileName = "items";
 class ItemLocalDataSourceImpl implements ItemsLocalDataSource {
   final FileGetter fileGetter;
 
   ItemLocalDataSourceImpl({required this.fileGetter});
   @override
   Future<List<ItemModel>> getItemsType() async {
-    final itemsJson = json.decode(await fileGetter.getFile("items.json"));
+    final itemsJson = json.decode(await fileGetter.getFile("$itemsFileName.json"));
     List<ItemModel> items = [];
     for (var item in itemsJson) {
       final itemModel = ItemModel.fromJson(item);
