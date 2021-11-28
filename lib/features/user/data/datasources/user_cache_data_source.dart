29261fc20a89d59fb0f0e5335417dfb2ba66c401
Moises <moisesalonso01@gmail.com>
2021-11-28 17:13:39 -0300

    feat: add saveContact method on the user datasource

--- lib/features/user/data/datasources/user_cache_data_source.dart
+++ lib/features/user/data/datasources/user_cache_data_source.dart
@@ -4,19 +4,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:the_resident_zombie/core/error/exceptions.dart';
 import 'package:the_resident_zombie/features/user/data/models/user_model.dart';
 
-abstract class UserCacheDataSource{
+abstract class UserCacheDataSource {
   /// Gets the cached [UserModel] which was saved after the successful registration.
-  /// 
+  ///
   /// Throws [CacheException] if no cached data is present.
   Future<UserModel> getUser();
 
   Future<void> cacheUser(UserModel userToCache);
+
+  Future<String> saveContact(String id);
 }
 
 const String CACHED_USER = 'CACHED_USER';
+const String CACHED_CONTACT = 'CACHED_CONTACT';
 
-
-class UserCacheDataSourceImpl implements UserCacheDataSource{
+class UserCacheDataSourceImpl implements UserCacheDataSource {
   final SharedPreferences sharedPreferences;
 
   UserCacheDataSourceImpl({required this.sharedPreferences});
@@ -27,14 +29,34 @@ class UserCacheDataSourceImpl implements UserCacheDataSource{
 
   @override
   Future<UserModel> getUser() async {
-     final jsonString = sharedPreferences.getString(CACHED_USER);
+    final jsonString = sharedPreferences.getString(CACHED_USER);
 
-    if(jsonString!=null){
+    if (jsonString != null) {
       return await Future.value(UserModel.fromJson(json.decode(jsonString)));
-    }else{
+    } else {
       throw CacheException();
     }
-
   }
 
+  @override
+  Future<String> saveContact(String id) async {
+    try {
+      final getStoredContacts = sharedPreferences.getString(CACHED_CONTACT);
+
+      String newContactString = "";
+      if (getStoredContacts != null) {
+        newContactString = "$getStoredContacts;$id";
+      } else {
+        newContactString = id;
+      }
+
+      final result = await sharedPreferences.setString(CACHED_CONTACT, newContactString);
+
+      if(!result) throw CacheException();
+
+      return id;
+    } catch (e) {
+      throw CacheException();
+    }
+  }
 }
