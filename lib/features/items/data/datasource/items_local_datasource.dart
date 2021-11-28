64f090274d9d3a454ce56155c89b7b55fac460b3
Moises <moisesalonso01@gmail.com>
2021-11-27 21:00:42 -0300

    feat: add datasource implementation

---
+++ lib/features/items/data/datasource/items_local_datasource.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:the_resident_zombie/core/error/failures.dart';
+import 'package:the_resident_zombie/core/platform/file_getter.dart';
+import 'package:the_resident_zombie/features/items/data/models/item_model.dart';
+import 'package:the_resident_zombie/features/items/domain/entities/item_entity.dart';
+
+abstract class ItemsLocalDataSource {
+  Future<List<ItemEntity>> getItemsType();
+}
+
+class ItemLocalDataSourceImpl implements ItemsLocalDataSource {
+  final FileGetter fileGetter;
+
+  ItemLocalDataSourceImpl({required this.fileGetter});
+  @override
+  Future<List<ItemModel>> getItemsType() async {
+    final itemsJson = json.decode(await fileGetter.getFile("items.json"));
+    List<ItemModel> items = [];
+    for (var item in itemsJson) {
+      final itemModel = ItemModel.fromJson(item);
+      items.add(itemModel);
+    }
+
+    return items;
+  }
+}
