40ae61e69f40f48ab2de6c18b16ea15a022c36b0
Moises <moisesalonso01@gmail.com>
2021-11-28 05:25:37 -0300

    refactor: now update position is returnig a UserModel

--- lib/features/user/domain/usecases/update_user_location_usecase.dart
+++ lib/features/user/domain/usecases/update_user_location_usecase.dart
@@ -4,16 +4,17 @@ import 'package:dartz/dartz.dart';
 import 'package:the_resident_zombie/core/usecases/usecase.dart';
 import 'package:the_resident_zombie/features/items/domain/usecase/get_items_type_usecase.dart';
 import 'package:the_resident_zombie/features/location/domain/usecases/get_location_usecase.dart';
+import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 import 'package:the_resident_zombie/features/user/domain/repositories/user_repository.dart';
 
-class UpdateUserLocationUsecase extends UseCase<bool, ParamsUpdateLocation> {
+class UpdateUserLocationUsecase extends UseCase<UserEntity, ParamsUpdateLocation> {
   final UserRepository repository;
   final GetLocationUseCase getLocationUsecase;
 
   UpdateUserLocationUsecase({required this.getLocationUsecase, required this.repository});
 
   @override
-  Future<Either<Failure, bool>> call(params) async {
+  Future<Either<Failure, UserEntity>> call(params) async {
 
     final locationEntityResponse = await getLocationUsecase(NoParamsGetLocation());
     if (locationEntityResponse.isLeft()) return Left(DeviceFailure());
