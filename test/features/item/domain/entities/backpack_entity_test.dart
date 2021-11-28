3d2c366ff838b7c8a90c9c09843e1057be857187
Moises <moisesalonso01@gmail.com>
2021-11-27 22:07:16 -0300

    test: update backpack entities to test constructor populate

--- test/features/item/domain/entities/backpack_entity_test.dart
+++ test/features/item/domain/entities/backpack_entity_test.dart
@@ -5,14 +5,23 @@ import 'package:the_resident_zombie/features/items/domain/entities/item_entity.d
 void main() {
   late BackPackEntity emptyBackPack;
   late BackPackEntity backPackWithItem;
-  late ItemEntity item;
+  late BackPackEntity backPackWithTwoItems;
+
+  late ItemEntity itemFiji;
+  late ItemEntity itemTest;
 
   setUp(() {
     emptyBackPack = BackPackEntity();
     backPackWithItem = BackPackEntity();
+    backPackWithTwoItems = BackPackEntity();
 
-    ItemEntity item = ItemEntity(name: 'Fiji Water', value: 14);
-    backPackWithItem.add(item);
+    itemFiji = ItemEntity(name: 'Fiji Water', value: 14);
+    itemTest = ItemEntity(name: 'test', value: 0);
+
+    backPackWithItem.add(itemFiji);
+    
+    backPackWithTwoItems.add(itemFiji);
+    backPackWithTwoItems.add(itemTest);
   });
 
   final tItem0 = ItemEntity(name: 'Fiji Water', value: 14);
@@ -20,26 +29,43 @@ void main() {
   final tItem2 = ItemEntity(name: 'Campbell Soup', value: 14);
   final String tString = "Fiji Water:2;Campbell Soup:1";
 
-  test(
-    'add [ItemEntity] to backpack',
-    () async {
-      // act
-      emptyBackPack.add(tItem0);
-      // assert
-      expect(emptyBackPack, equals(backPackWithItem));
-      expect(emptyBackPack.items.length, 1);
-    },
-  );
+  group(
+    'backpack entity ',
+    () {
+      test(
+        'add [ItemEntity] to backpack',
+        () async {
+          // act
+          emptyBackPack.add(tItem0);
+          // assert
+          expect(emptyBackPack, equals(backPackWithItem));
+          expect(emptyBackPack.items.length, 1);
+        },
+      );
 
-  test(
-    'get right String format from toString method on backpack ',
-    () async {
-      // act
-      emptyBackPack.add(tItem0);
-      emptyBackPack.add(tItem1);
-      emptyBackPack.add(tItem2);
-      // assert
-      expect(emptyBackPack.toString(), equals(tString));
+      test(
+        'get right String format from toString method on backpack ',
+        () async {
+          // act
+          emptyBackPack.add(tItem0);
+          emptyBackPack.add(tItem1);
+          emptyBackPack.add(tItem2);
+          // assert
+          expect(emptyBackPack.toString(), equals(tString));
+        },
+      );
+
+      test(
+        'get a backpack population with the constructor',
+        () async {
+          // act
+          emptyBackPack.add(itemFiji);
+          emptyBackPack.add(itemTest);
+
+          // assert
+          expect(emptyBackPack, equals(backPackWithTwoItems));
+        },
+      );
     },
   );
 }
