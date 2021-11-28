e770f19b2ee3a7c5113e2c5a800e7649bcaf7a29
Moises <moisesalonso01@gmail.com>
2021-11-28 04:48:45 -0300

    feat: add update location usecase implementation

---
+++ lib/features/user/domain/usecases/update_user_location_usecase.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+import 'package:the_resident_zombie/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:the_resident_zombie/core/usecases/usecase.dart';
+import 'package:the_resident_zombie/features/items/domain/usecase/get_items_type_usecase.dart';
+import 'package:the_resident_zombie/features/location/domain/usecases/get_location_usecase.dart';
+import 'package:the_resident_zombie/features/user/domain/repositories/user_repository.dart';
+
+class UpdateUserLocationUsecase extends UseCase<bool, ParamsUpdateLocation> {
+  final UserRepository repository;
+  final GetLocationUseCase getLocationUsecase;
+
+  UpdateUserLocationUsecase({required this.getLocationUsecase, required this.repository});
+
+  @override
+  Future<Either<Failure, bool>> call(params) async {
+
+    final locationEntityResponse = await getLocationUsecase(NoParamsGetLocation());
+    if (locationEntityResponse.isLeft()) return Left(DeviceFailure());
+    final locationEntity = locationEntityResponse.toOption().toNullable()!;
+
+    return await repository.updateUserLocation(params.identifier, locationEntity.toStringAsCoordinated());
+  }
+}
+
+class ParamsUpdateLocation extends Equatable {
+  final String identifier;
+
+  ParamsUpdateLocation({required this.identifier});
+  @override
+  List<Object> get props => [];
+}
