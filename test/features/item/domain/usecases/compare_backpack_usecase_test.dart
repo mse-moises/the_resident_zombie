a0d1e90885eb7214877f06e037ead1c18c0afa91
Moises <moisesalonso01@gmail.com>
2021-11-29 03:34:49 -0300

    test: add test for 'compare backpack' usecase

---
+++ test/features/item/domain/usecases/compare_backpack_usecase_test.dart
@@ -0,0 +1,52 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:the_resident_zombie/features/items/domain/entities/back_pack_entity.dart';
+import 'package:the_resident_zombie/features/items/domain/entities/item_entity.dart';
+import 'package:the_resident_zombie/features/items/domain/usecase/compare_backpack_usecase.dart';
+import 'package:the_resident_zombie/features/items/domain/usecase/get_backpack_from_numbers_usecase.dart';
+
+void main() {
+  late CompareBackpackUsecase usecase;
+  late BackPackEntity higherValue;
+  late BackPackEntity lowerValue;
+  setUp(() {
+    usecase = CompareBackpackUsecase();
+    higherValue = BackPackEntity(startWithItems: [ItemEntity(name:"test",value:10),ItemEntity(name:"test",value:10)]);
+    lowerValue = BackPackEntity(startWithItems: [ItemEntity(name:"test",value:5),ItemEntity(name:"test",value:5),ItemEntity(name:"test",value:5)]);
+  });
+
+  group(
+    'Compare Backpack Usecase:',
+    () {
+      test(
+        'should return a 5',
+        () async {
+          // act
+          final result = await usecase(CompareBackPackParams(higherValue,lowerValue));
+          // assert
+          expect(result, Right(5));
+        },
+      );
+
+      test(
+        'should return a -5',
+        () async {
+          // act
+          final result = await usecase(CompareBackPackParams(lowerValue,higherValue));
+          // assert
+          expect(result, Right(-5));
+        },
+      );
+
+      test(
+        'should return a 0',
+        () async {
+          // act
+          final result = await usecase(CompareBackPackParams(higherValue,higherValue));
+          // assert
+          expect(result, Right(0));
+        },
+      );
+    },
+  );
+}
