d1a89b4226f71371390d9d6a3ec65ad1b12e797e
Moises <moisesalonso01@gmail.com>
2021-11-29 03:57:26 -0300

    feat: add 'backpackcomparation' failure

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -13,3 +13,9 @@ class ServerFailure extends Failure {}
 class CacheFailure extends Failure {}
 
 class DeviceFailure extends Failure {}
+
+class BackPackComparationFailure extends Failure {
+  final int difference;
+
+  BackPackComparationFailure(this.difference);
+}
