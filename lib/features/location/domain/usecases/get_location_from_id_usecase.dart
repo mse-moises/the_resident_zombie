356d64b50f27e7f82096456ddd3944a089c40768
Moises <moisesalonso01@gmail.com>
2021-11-29 18:39:42 -0300

    feat: add implementation 'get location by id' usecase

---
+++ lib/features/location/domain/usecases/get_location_from_id_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+import 'package:the_resident_zombie/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:the_resident_zombie/core/usecases/usecase.dart';
+import 'package:the_resident_zombie/features/location/domain/entities/location_entity.dart';
+import 'package:the_resident_zombie/features/location/domain/repositories/location_repository.dart';
+import 'package:the_resident_zombie/features/user/domain/repositories/user_repository.dart';
+
+class GetLocationFromIdUseCase
+    implements UseCase<LocationEntity, GetLocationFromIdParams> {
+      final LocationRepository repository;
+
+  GetLocationFromIdUseCase({required this.repository});
+  @override
+  Future<Either<Failure, LocationEntity>> call(params) async{
+    return await repository.GetLocationFromId(params.identifier);
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
+
+class GetLocationFromIdParams extends Equatable {
+  final String identifier;
+
+  GetLocationFromIdParams(this.identifier);
+  @override
+  List<Object> get props => [identifier];
+}
