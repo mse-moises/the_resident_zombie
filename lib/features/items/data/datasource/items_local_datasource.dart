bcd3640951844ecb5fcb91a21d43e82ad0ef553b
Moises <moisesalonso01@gmail.com>
2021-11-30 02:45:32 -0300

    fix: fix json convert

--- lib/features/items/data/datasource/items_local_datasource.dart
+++ lib/features/items/data/datasource/items_local_datasource.dart
@@ -1,7 +1,4 @@
 import 'dart:convert';
-
-import 'package:dartz/dartz.dart';
-import 'package:the_resident_zombie/core/error/failures.dart';
 import 'package:the_resident_zombie/core/platform/file_getter.dart';
 import 'package:the_resident_zombie/features/items/data/models/item_model.dart';
 import 'package:the_resident_zombie/features/items/domain/entities/item_entity.dart';
@@ -11,13 +8,15 @@ abstract class ItemsLocalDataSource {
 }
 
 const String itemsFileName = "items";
+
 class ItemLocalDataSourceImpl implements ItemsLocalDataSource {
   final FileGetter fileGetter;
 
   ItemLocalDataSourceImpl({required this.fileGetter});
   @override
   Future<List<ItemModel>> getItemsType() async {
-    final itemsJson = json.decode(await fileGetter.getFile("$itemsFileName.json"));
+    final itemsJson =
+        json.decode(await fileGetter.getFile("$itemsFileName.json"));
     List<ItemModel> items = [];
     for (var item in itemsJson) {
       final itemModel = ItemModel.fromJson(item);
