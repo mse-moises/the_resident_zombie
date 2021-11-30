d5831acc1ac62c254124292bcba8cfb792008abc
Moises <moisesalonso01@gmail.com>
2021-11-30 06:09:55 -0300

    feat: add text theme and named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:the_resident_zombie/features/main/presentation/pages/splash_page/pages/Splash_page.dart';
+import 'package:the_resident_zombie/features/user/presentation/crate_user_page.dart';
+
+import 'package:the_resident_zombie/initial_configs.dart';
 
 import 'injection_container.dart' as di;
 
 void main() async {
+  await initialConfigs();
   await di.init();
   runApp(const MyApp());
 }
@@ -17,8 +23,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.nunitoTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
-      routes: {},
+      routes: {
+        SplashPage.route: (_) => SplashPage(),
+        CreateUserPage.route: (_) => CreateUserPage(),
+      },
     );
   }
 }
