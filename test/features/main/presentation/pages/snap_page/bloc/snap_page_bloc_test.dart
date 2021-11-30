a5fdbc96ca08740c589d8e2c574edc52bfd522b8
Moises <moisesalonso01@gmail.com>
2021-11-30 00:01:45 -0300

    test: add test to the 'main' feature bloc

---
+++ test/features/main/presentation/pages/snap_page/bloc/snap_page_bloc_test.dart
@@ -0,0 +1,62 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:the_resident_zombie/core/error/failures.dart';
+import 'package:the_resident_zombie/features/main/presentation/pages/snap_page/bloc/snap_page_bloc.dart';
+import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
+import 'package:the_resident_zombie/features/user/domain/usecases/get_local_user_usecase.dart';
+
+import 'snap_page_bloc_test.mocks.dart';
+
+@GenerateMocks([GetLocalUserUseCase])
+void main() {
+  late MockGetLocalUserUseCase getLocalUserUsecase;
+  late SnapPageBloc bloc;
+
+  setUp(() {
+    getLocalUserUsecase = MockGetLocalUserUseCase();
+    bloc = SnapPageBloc(getLocalUser: getLocalUserUsecase);
+  });
+
+  group(
+    'SnapPageBloc:',
+    () {
+      final tUserEntity = UserEntity(age: 30, gender: 't', name: 'test');
+      test(
+        'intial state is [SnapPageInitial]',
+        () async {
+          // assert
+          expect(bloc.state, equals(SnapPageInitial()));
+        },
+      );
+
+      blocTest<SnapPageBloc,SnapPageState>(
+        'emit [SnapPageUserFound] if found',
+        build: () {
+          when(getLocalUserUsecase(any))
+              .thenAnswer((_) async => Right(tUserEntity));
+          return bloc;
+        },
+        act: (bloc) => bloc.add(SearchLocalUserEvent()),
+        expect: () => [
+          SnapPageUserFound(),
+        ],
+      );
+
+      blocTest<SnapPageBloc,SnapPageState>(
+        'emit [SnapPageUserNotFound] if not found',
+        build: () {
+          when(getLocalUserUsecase(any))
+              .thenAnswer((_) async => Left(DeviceFailure()));
+          return bloc;
+        },
+        act: (bloc) => bloc.add(SearchLocalUserEvent()),
+        expect: () => [
+          SnapPageUserNotFound(),
+        ],
+      );
+    },
+  );
+}
