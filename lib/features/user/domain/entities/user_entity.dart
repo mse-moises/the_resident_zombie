0b9d6670e413cdf11011fbc25e17db4c6b64b91a
Moises <moisesalonso01@gmail.com>
2021-11-30 21:51:20 -0300

    feat: add id in 'user entity'

--- lib/features/user/domain/entities/user_entity.dart
+++ lib/features/user/domain/entities/user_entity.dart
@@ -1,12 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 class UserEntity extends Equatable {
+  final String id;
   final String name;
   final int age;
   final String gender;
 
-  UserEntity({required this.name, required this.age, required this.gender});
+  UserEntity({required this.name, required this.age, required this.gender, required this.id});
 
   @override
-  List<Object> get props => [name, age, gender];
+  List<Object> get props => [name, age, gender,id];
 }
