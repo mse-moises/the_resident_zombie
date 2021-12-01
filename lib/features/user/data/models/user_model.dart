56d290788a174a53aaf1872d5811c5d008ee7aed
Moises <moisesalonso01@gmail.com>
2021-12-01 06:07:48 -0300

    feat: add flag 'infected' in the user model

--- lib/features/user/data/models/user_model.dart
+++ lib/features/user/data/models/user_model.dart
@@ -1,27 +1,31 @@
 import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 
 class UserModel extends UserEntity {
-  UserModel({required String name, required int age, required String gender, required String id})
-      : super(name:name, age:age, gender:gender, id: id);
+  UserModel({required String name, required int age, required String gender, required String id, required bool infected})
+      : super(name:name, age:age, gender:gender, id: id, infected:infected);
 
   factory UserModel.fromJson(Map<String,dynamic> json){
     final name = json['name'];
     final age = json['age'];
     final gender = json['gender'];
     final id = json['id'];
+    final infected = json['infected'];
     return UserModel(
       id:id,
       name:name,
       age:age,
       gender:gender,
+      infected:infected,
     );
   }
 
   Map<String,dynamic> toJson(){
     return{
-      "name":"$name",
+      "id":id,
+      "name":name,
       "age":age,
-      "gender":"$gender"
+      "gender":gender,
+      "infected":infected,
     };
   }
 
