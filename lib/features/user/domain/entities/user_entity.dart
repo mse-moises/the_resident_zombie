954c93864d0f1987f7d66007d42dfd79b441bfd2
Moises <moisesalonso01@gmail.com>
2021-12-01 06:08:18 -0300

    feat: add flag 'infected' in the user entity

--- lib/features/user/domain/entities/user_entity.dart
+++ lib/features/user/domain/entities/user_entity.dart
@@ -5,9 +5,10 @@ class UserEntity extends Equatable {
   final String name;
   final int age;
   final String gender;
+  final bool infected;
 
-  UserEntity({required this.name, required this.age, required this.gender, required this.id});
+  UserEntity({required this.name, required this.age, required this.gender, required this.id, required this.infected});
 
   @override
-  List<Object> get props => [name, age, gender,id];
+  List<Object> get props => [name, age, gender,id, infected];
 }
