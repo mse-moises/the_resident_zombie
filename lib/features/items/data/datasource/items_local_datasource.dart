0ab172d7709bee8a9f18db3393995fd45a2c6f56
Moises <moisesalonso01@gmail.com>
2021-12-01 06:03:31 -0300

    refactor: refactor the file getter in 'items local datasource'

--- lib/features/items/data/datasource/items_local_datasource.dart
+++ lib/features/items/data/datasource/items_local_datasource.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
+import 'package:the_resident_zombie/core/error/exceptions.dart';
 import 'package:the_resident_zombie/core/platform/file_getter.dart';
 import 'package:the_resident_zombie/features/items/data/models/item_model.dart';
 import 'package:the_resident_zombie/features/items/domain/entities/item_entity.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 abstract class ItemsLocalDataSource {
   Future<List<ItemEntity>> getItemsType();
 }
 
-const String itemsFileName = "items";
+const String itemsFileName = "assets/json/items";
 
 class ItemLocalDataSourceImpl implements ItemsLocalDataSource {
   final FileGetter fileGetter;
@@ -15,8 +17,15 @@ class ItemLocalDataSourceImpl implements ItemsLocalDataSource {
   ItemLocalDataSourceImpl({required this.fileGetter});
   @override
   Future<List<ItemModel>> getItemsType() async {
-    final itemsJson =
-        json.decode(await fileGetter.getFile("$itemsFileName.json"));
+    late List itemsJson;
+    try {
+      itemsJson =  json.decode(await fileGetter.getFile(itemsFileName));
+      //json.decode(await rootBundle.loadString(
+          //"$itemsFileName.json"));
+    } catch (e) {
+      print(e);
+      throw DeviceException();
+    }
     List<ItemModel> items = [];
     for (var item in itemsJson) {
       final itemModel = ItemModel.fromJson(item);
