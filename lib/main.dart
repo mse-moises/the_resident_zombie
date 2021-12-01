2c7ea20c5e38abd54ee6edc7c5d07583b4878920
Moises <moisesalonso01@gmail.com>
2021-12-01 06:18:06 -0300

    fix: fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:the_resident_zombie/features/main/presentation/pages/home_page/pages/home_page.dart';
 import 'package:the_resident_zombie/features/main/presentation/pages/splash_page/pages/Splash_page.dart';
-import 'package:the_resident_zombie/features/user/presentation/crate_user_page.dart';
+import 'package:the_resident_zombie/features/user/presentation/contact_list/pages/contact_list_page.dart';
+import 'package:the_resident_zombie/features/user/presentation/contact_list/pages/qr_code_page.dart';
+import 'package:the_resident_zombie/features/user/presentation/create_user/page/crate_user_page.dart';
+import 'package:the_resident_zombie/core/utils/style.dart' as style;
 
 import 'package:the_resident_zombie/initial_configs.dart';
 
@@ -26,10 +30,19 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.nunitoTextTheme(
           Theme.of(context).textTheme,
         ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: style.lightGrey,
+        ),
       ),
       routes: {
         SplashPage.route: (_) => SplashPage(),
         CreateUserPage.route: (_) => CreateUserPage(),
+        HomePage.route: (_) => HomePage(),
+        ContactList.route: (_) => ContactList(),
+        QRCodeReaderPage.route: (_) => QRCodeReaderPage(),
       },
     );
   }
