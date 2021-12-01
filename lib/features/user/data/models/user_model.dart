2b2faaedd201d745a5a94669849ae979cb17f1c8
Moises <moisesalonso01@gmail.com>
2021-11-30 21:51:43 -0300

    feat: add id in 'user model'

--- lib/features/user/data/models/user_model.dart
+++ lib/features/user/data/models/user_model.dart
@@ -1,14 +1,16 @@
 import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 
 class UserModel extends UserEntity {
-  UserModel({required String name, required int age, required String gender})
-      : super(name:name, age:age, gender:gender);
+  UserModel({required String name, required int age, required String gender, required String id})
+      : super(name:name, age:age, gender:gender, id: id);
 
   factory UserModel.fromJson(Map<String,dynamic> json){
     final name = json['name'];
     final age = json['age'];
     final gender = json['gender'];
+    final id = json['id'];
     return UserModel(
+      id:id,
       name:name,
       age:age,
       gender:gender,
