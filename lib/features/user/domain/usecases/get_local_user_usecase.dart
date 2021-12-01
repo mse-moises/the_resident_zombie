6750e19dab5c0d64c78f8ad40a48ed30a2db3015
Moises <moisesalonso01@gmail.com>
2021-12-01 06:09:19 -0300

    fix: fix bug that doesnt tell if the local user is infected or not

--- lib/features/user/domain/usecases/get_local_user_usecase.dart
+++ lib/features/user/domain/usecases/get_local_user_usecase.dart
@@ -4,14 +4,23 @@ import 'package:dartz/dartz.dart';
 import 'package:the_resident_zombie/core/usecases/usecase.dart';
 import 'package:the_resident_zombie/features/user/domain/entities/user_entity.dart';
 import 'package:the_resident_zombie/features/user/domain/repositories/user_repository.dart';
+import 'package:the_resident_zombie/features/user/domain/usecases/get_user_by_id_usecase.dart';
 
 class GetLocalUserUseCase implements UseCase<UserEntity, GetLocalUserNoParams> {
   final UserRepository repository;
 
-  GetLocalUserUseCase({required this.repository});
+  final GetUserByIdUseCase getUserById;
+
+  GetLocalUserUseCase({required this.repository, required this.getUserById});
   @override
   Future<Either<Failure, UserEntity>> call(params) async {
-    return await repository.getLocalUser();
+    final result = await repository.getLocalUser();
+    if (result.isLeft()) return result;
+
+    late UserEntity user;
+    result.fold((l) => null, (r) => user = r);
+
+    return await getUserById(GetUserByIdParams(identifier: user.id));
   }
 }
 
